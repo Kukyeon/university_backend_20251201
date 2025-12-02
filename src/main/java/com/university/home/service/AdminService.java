2993f2390e22ac91f8360d6e8f0749c2598139b6
ddujeong <qoddl159@nate.com>
2025-12-02 12:10:55 +0900

    등록금 관련 서비스CRUD

--- src/main/java/com/university/home/service/AdminService.java
+++ src/main/java/com/university/home/service/AdminService.java
@@ -1,11 +1,13 @@
 package com.university.home.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.university.home.dto.CollTuitFormDto;
-import com.university.home.entity.College;
+import com.university.home.entity.CollTuit;
 import com.university.home.exception.CustomRestfullException;
 import com.university.home.repository.CollTuitRepository;
 
@@ -17,11 +19,47 @@ public class AdminService {
 	@Autowired
 	CollTuitRepository collTuitRepository;
 	
+	//===========등록금 관련 메서드================
 	@Transactional
-	public void createCollTuit(CollTuitFormDto  collTuitFormDto) {
-		if (collTuitRepository.existsByCollegeId(collTuitFormDto.getCollegeId())) {
-	        throw new CustomRestfullException("이미 등록금이 입력된 학과입니다", HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
+	public void createCollTuit(CollTuitFormDto  dto) {
+		 if (collTuitRepository.existsByCollegeId(dto.getCollegeId())) {
+	            throw new CustomRestfullException("이미 등록금이 입력된 학과입니다", HttpStatus.INTERNAL_SERVER_ERROR);
+	        }
 
+	        // DTO → Entity 변환 후 저장
+	        CollTuit collTuit = new CollTuit();
+	        collTuit.setCollegeId(dto.getCollegeId());
+	        collTuit.setAmount(dto.getAmount());
+
+	        collTuitRepository.save(collTuit);
+	}
+	public List<CollTuitFormDto> getCollTuit() {
+		List<CollTuit> collTuitList =collTuitRepository.findAll();
+		
+		List<CollTuitFormDto> dtoList = collTuitList.stream()
+			        .map(c -> {
+			            CollTuitFormDto dto = new CollTuitFormDto();
+			            dto.setCollegeId(c.getCollegeId());
+			            dto.setAmount(c.getAmount());
+			            return dto;
+			        })
+			        .toList();
+
+	   return dtoList;
+	}
+	@Transactional
+	public void deleteCollTuit(Long collegeId) {
+		CollTuit collTuit = collTuitRepository.findById(collegeId)
+		        .orElseThrow(() -> new 
+		        		CustomRestfullException("해당 학과 등록금이 존재하지 않습니다.", HttpStatus.NOT_FOUND));
+		collTuitRepository.delete(collTuit);
+	}
+	@Transactional
+	public CollTuit updateCollTuit(CollTuitFormDto dto) {
+		CollTuit collTuit = collTuitRepository.findById(dto.getCollegeId())
+		        .orElseThrow(() -> new 
+		        		CustomRestfullException("해당 학과 등록금이 존재하지 않습니다.", HttpStatus.NOT_FOUND));
+		collTuit.setAmount(dto.getAmount());
+		return collTuit;
 	}
 }
