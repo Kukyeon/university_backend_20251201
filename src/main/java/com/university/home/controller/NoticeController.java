506251378bded457f9e612c60fc27e27d3a25d38
Kukyeon <rnrdus2000@naver.com>
2025-12-02 10:32:00 +0900

    Notice 임시 수정

--- src/main/java/com/university/home/controller/NoticeController.java
+++ src/main/java/com/university/home/controller/NoticeController.java
@@ -2,11 +2,9 @@ package com.university.home.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.university.home.dto.NoticeFormDto;
 import com.university.home.dto.NoticePageFormDto;
@@ -23,24 +21,36 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
+    // 공지사항 목록
     @GetMapping("")
-    public String noticeList(Model model, @RequestParam(defaultValue = "select") String crud) {
+    public String noticeList(Model model,
+                             @RequestParam(defaultValue = "select") String crud,
+                             @RequestParam(defaultValue = "0") Integer page,
+                             @RequestParam(required = false) String keyword,
+                             @RequestParam(required = false) String type) {
+
         model.addAttribute("crud", crud);
+
         NoticePageFormDto dto = new NoticePageFormDto();
-        dto.setPage(0);
+        dto.setPage(page);
+        dto.setKeyword(keyword);
+        dto.setType(type);
 
         List<Notice> noticeList = noticeService.getNotices(dto);
         model.addAttribute("listCount", Math.ceil(noticeList.size() / 10.0));
         model.addAttribute("noticeList", noticeList.isEmpty() ? null : noticeList);
+
         return "/board/notice";
     }
 
+    // 공지사항 작성
     @PostMapping("/write")
     public String createNotice(NoticeFormDto dto) {
         noticeService.createNotice(dto);
         return "redirect:/notice";
     }
 
+    // 공지사항 읽기
     @GetMapping("/read")
     public String readNotice(Model model, @RequestParam Long id) {
         model.addAttribute("crud", "read");
@@ -48,11 +58,14 @@ public class NoticeController {
 
         Notice notice = noticeService.getNoticeById(id);
         model.addAttribute("notice", notice);
+
+        // 줄바꿈 처리
         notice.setContent(notice.getContent().replace("\r\n", "<br>"));
 
         return "/board/notice";
     }
 
+    // 공지사항 삭제
     @GetMapping("/delete")
     public String deleteNotice(@RequestParam Long id) {
         noticeService.deleteNotice(id);
