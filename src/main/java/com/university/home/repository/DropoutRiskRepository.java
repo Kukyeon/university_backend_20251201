eb959b4710b50b7454b1681c0e13d43bb0efb709
LSM-1020 <smmlee@naver.com>
2025-12-02 17:50:48 +0900

    위험 학생 대쉬보드

--- src/main/java/com/university/home/repository/DropoutRiskRepository.java
+++ src/main/java/com/university/home/repository/DropoutRiskRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.university.home.entity.DropoutRisk;
 
 public interface DropoutRiskRepository extends JpaRepository<DropoutRisk, Long>{
-	// 특정 학생의 위험 분석 이력 조회
-    List<DropoutRisk> findByStudentIdOrderByAnalyzedDateDesc(Long studentId);
+	// [핵심 기능] 특정 학과의 위험군 학생 목록 조회
+    // 해석: DropoutRisk 안의 student 안의 department 안의 id가 일치하는 것 찾기
+    List<DropoutRisk> findByStudentDepartmentIdOrderByRiskScoreDesc(Long deptId);
+
+    // [관리자용] 전체 학과의 '위험' 등급 학생만 조회
+    List<DropoutRisk> findByRiskLevelOrderByRiskScoreDesc(String riskLevel);
     
-    // [관리자용] 위험도가 높은 학생들만 조회 (예: '위험' 등급)
-    List<DropoutRisk> findByRiskLevel(String riskLevel);
+    // [통계용] 특정 학과의 위험(Level) 학생 수 카운트
+    long countByStudentDepartmentIdAndRiskLevel(Long deptId, String riskLevel);
 	
 }
