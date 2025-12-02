5e2b383ffc9819bab7c193d8ec3fb304d1d18465
ddujeong <qoddl159@nate.com>
2025-12-02 17:21:17 +0900

    gk...

--- src/main/java/com/university/home/repository/CollTuitRepository.java
+++ src/main/java/com/university/home/repository/CollTuitRepository.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.university.home.entity.CollTuit;
 import java.util.List;
+import java.util.Optional;
+
+import com.university.home.entity.College;
+
 
 
 public interface CollTuitRepository extends JpaRepository<CollTuit, Long>{
 
 	boolean existsByCollege_Id(Long collegeId);
 	
-	//findByCollegeIdAnd(Long collegeId);
+	Optional<CollTuit> findByCollege(College college);
 }
