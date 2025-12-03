dcb076f4dd93fe85abe57736d201aa735773bad3
ddujeong <qoddl159@nate.com>
2025-12-03 11:04:00 +0900

    장학금 일단 만들었움..

--- src/main/java/com/university/home/repository/StuSchRepository.java
+++ src/main/java/com/university/home/repository/StuSchRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.university.home.entity.StuSch;
+import com.university.home.entity.Student;
 
 public interface StuSchRepository extends JpaRepository<StuSch, Long> {
 
-	List<StuSch> findByStudentId(Long studentId);
+	List<StuSch> findByStudent(Student student);
 	
 	List<StuSch> findByStudentIdAndSchYearAndSemester(Long studentId, Long schYear, Long semester);
 }
