4aa3a551eff49deb3e282dd3a86a15d03ce016c5
LSM-1020 <smmlee@naver.com>
2025-12-04 11:54:54 +0900

    교수<->학생 관계 수정 및 학생에게도 상담알림 전송

--- src/main/java/com/university/home/repository/DropoutRiskRepository.java
+++ src/main/java/com/university/home/repository/DropoutRiskRepository.java
@@ -1,6 +1,7 @@
 package com.university.home.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,10 +12,11 @@ public interface DropoutRiskRepository extends JpaRepository<DropoutRisk, Long>{
     // 해석: DropoutRisk 안의 student 안의 department 안의 id가 일치하는 것 찾기
     List<DropoutRisk> findByStudentDepartmentIdOrderByRiskScoreDesc(Long deptId);
 
-    // [관리자용] 전체 학과의 '위험' 등급 학생만 조회
+    // [관리자용] 전체 학과의 '심각' 등급 학생만 조회
     List<DropoutRisk> findByRiskLevelOrderByRiskScoreDesc(String riskLevel);
     
     // [통계용] 특정 학과의 위험(Level) 학생 수 카운트
     long countByStudentDepartmentIdAndRiskLevel(Long deptId, String riskLevel);
 	
+    Optional<DropoutRisk> findTopByStudentIdOrderByAnalyzedDateDesc(Long studentId);
 }
