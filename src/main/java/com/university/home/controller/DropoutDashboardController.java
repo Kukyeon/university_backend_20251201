e96cbd0ecd4302be20189afe286cfb35ed4aded3
LSM-1020 <smmlee@naver.com>
2025-12-05 09:38:02 +0900

    @requestparam professorId

--- src/main/java/com/university/home/controller/DropoutDashboardController.java
+++ src/main/java/com/university/home/controller/DropoutDashboardController.java
@@ -18,7 +18,7 @@ public class DropoutDashboardController {
     // [API] 교수님 대시보드 데이터 조회
     // 요청: GET http://localhost:8888/api/dashboard/risk-list?professorId=101
     @GetMapping("/risk-list")
-    public ResponseEntity<List<DropoutRiskResponseDto>> getRiskList(@RequestParam Long professorId) {
+    public ResponseEntity<List<DropoutRiskResponseDto>> getRiskList(@RequestParam("professorId") Long professorId) {
         List<DropoutRiskResponseDto> list = dashboardService.getProfessorDashboard(professorId);
         return ResponseEntity.ok(list);
     }
