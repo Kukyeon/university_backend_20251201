e9b2f5a180146221c1bf9adcbea2cebe8826dc4a
LSM-1020 <smmlee@naver.com>
2025-12-05 14:43:29 +0900

    관리자 전체 위험학생 대쉬보드

--- src/main/java/com/university/home/controller/DropoutDashboardController.java
+++ src/main/java/com/university/home/controller/DropoutDashboardController.java
@@ -2,9 +2,11 @@ package com.university.home.controller;
 
 import com.university.home.dto.DropoutRiskResponseDto;
 import com.university.home.repository.DropoutRiskRepository;
+import com.university.home.service.CustomUserDetails;
 import com.university.home.service.DropoutDashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,7 +21,9 @@ public class DropoutDashboardController {
     // [API] 교수님 대시보드 데이터 조회
     // 요청: GET http://localhost:8888/api/dashboard/risk-list?professorId=101
     @GetMapping("/risk-list")
-    public ResponseEntity<List<DropoutRiskResponseDto>> getRiskList(@RequestParam("professorId") Long professorId) {
+    public ResponseEntity<List<DropoutRiskResponseDto>> getRiskList(@AuthenticationPrincipal CustomUserDetails loginUser
+) {
+    	Long professorId = loginUser.getUser().getId();
         List<DropoutRiskResponseDto> list = dashboardService.getProfessorDashboard(professorId);
         return ResponseEntity.ok(list);
     }
@@ -27,5 +31,13 @@ public class DropoutDashboardController {
     public ResponseEntity<String> deleteRisk(@PathVariable("id") Long id) {
         dropoutRiskRepository.deleteById(id);
         return ResponseEntity.ok("삭제되었습니다.");
+    }
+    
+ // [추가] 관리자용 전체 리스트 API
+    // GET http://localhost:8888/api/dashboard/admin/risk-list
+    @GetMapping("/admin/risk-list")
+    public ResponseEntity<List<DropoutRiskResponseDto>> getAllRiskList() {
+        // (나중에 시큐리티가 적용되면 여기서 'STAFF' 권한인지 체크하면 됩니다)
+        return ResponseEntity.ok(dashboardService.getAllRiskStudents());
     }
 }
