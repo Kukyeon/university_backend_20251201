7dc7d90de12a7769ad5b9af85ad359a5dd229b73
LSM-1020 <smmlee@naver.com>
2025-12-17 15:05:49 +0900

    @EnableScheduling 추가

--- src/main/java/com/university/home/University20251201Application.java
+++ src/main/java/com/university/home/University20251201Application.java
@@ -3,8 +3,9 @@ package com.university.home;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication
 @EntityScan("com.university.home.entity")
 public class University20251201Application {
