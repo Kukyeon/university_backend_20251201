cdcac2540c0ec4942ffd2c03a281e5d88985fdb6
LSM-1020 <smmlee@naver.com>
2025-12-19 16:57:40 +0900

    시간 설정

--- src/main/java/com/university/home/University20251201Application.java
+++ src/main/java/com/university/home/University20251201Application.java
@@ -1,9 +1,13 @@
 package com.university.home;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jakarta.annotation.PostConstruct;
 
 @EnableScheduling
 @SpringBootApplication
@@ -13,5 +17,11 @@ public class University20251201Application {
 	public static void main(String[] args) {
 		SpringApplication.run(University20251201Application.class, args);
 	}
+	@PostConstruct
+    public void init() {
+        // "내 시간은 무조건 서울이다" 선언
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("현재 시간대: " + TimeZone.getDefault().getID());
+    }
 
 }
